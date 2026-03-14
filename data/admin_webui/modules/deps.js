3383369b4725defcc01e5b5a15a40762e796c480
RichardLiu <2645345468@qq.com>
2026-03-15 05:35:57 +0800

    处理窄屏商品详情适配问题」} PMID? need remove end

--- data/admin_webui/modules/deps.js
+++ data/admin_webui/modules/deps.js
@@ -6,41 +6,37 @@ import React, {
 } from 'https://esm.sh/react@18.3.1'
 import { createRoot } from 'https://esm.sh/react-dom@18.3.1/client'
 import htm from 'https://esm.sh/htm@3.1.1'
-import {
-	ThemeProvider,
-	createTheme
-} from 'https://esm.sh/@mui/material@5.16.14/styles?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
-import {
-	Alert,
-	AppBar,
-	Box,
-	Button,
-	Card,
-	CardContent,
-	Chip,
-	CircularProgress,
-	CssBaseline,
-	Dialog,
-	DialogActions,
-	DialogContent,
-	DialogTitle,
-	Drawer,
-	LinearProgress,
-	MenuItem,
-	Snackbar,
-	Stack,
-	Switch,
-	Table,
-	TableBody,
-	TableCell,
-	TableContainer,
-	TableHead,
-	TableRow,
-	TextField,
-	Toolbar,
-	Typography,
-	useMediaQuery
-} from 'https://esm.sh/@mui/material@5.16.14?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import ThemeProvider from 'https://esm.sh/@mui/material@5.16.14/styles/ThemeProvider?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import createTheme from 'https://esm.sh/@mui/material@5.16.14/styles/createTheme?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Alert from 'https://esm.sh/@mui/material@5.16.14/Alert?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import AppBar from 'https://esm.sh/@mui/material@5.16.14/AppBar?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Box from 'https://esm.sh/@mui/material@5.16.14/Box?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Button from 'https://esm.sh/@mui/material@5.16.14/Button?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Card from 'https://esm.sh/@mui/material@5.16.14/Card?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import CardContent from 'https://esm.sh/@mui/material@5.16.14/CardContent?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Chip from 'https://esm.sh/@mui/material@5.16.14/Chip?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import CircularProgress from 'https://esm.sh/@mui/material@5.16.14/CircularProgress?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import CssBaseline from 'https://esm.sh/@mui/material@5.16.14/CssBaseline?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Dialog from 'https://esm.sh/@mui/material@5.16.14/Dialog?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import DialogActions from 'https://esm.sh/@mui/material@5.16.14/DialogActions?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import DialogContent from 'https://esm.sh/@mui/material@5.16.14/DialogContent?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import DialogTitle from 'https://esm.sh/@mui/material@5.16.14/DialogTitle?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Drawer from 'https://esm.sh/@mui/material@5.16.14/Drawer?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import LinearProgress from 'https://esm.sh/@mui/material@5.16.14/LinearProgress?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import MenuItem from 'https://esm.sh/@mui/material@5.16.14/MenuItem?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Snackbar from 'https://esm.sh/@mui/material@5.16.14/Snackbar?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Stack from 'https://esm.sh/@mui/material@5.16.14/Stack?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Switch from 'https://esm.sh/@mui/material@5.16.14/Switch?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Table from 'https://esm.sh/@mui/material@5.16.14/Table?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import TableBody from 'https://esm.sh/@mui/material@5.16.14/TableBody?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import TableCell from 'https://esm.sh/@mui/material@5.16.14/TableCell?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import TableContainer from 'https://esm.sh/@mui/material@5.16.14/TableContainer?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import TableHead from 'https://esm.sh/@mui/material@5.16.14/TableHead?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import TableRow from 'https://esm.sh/@mui/material@5.16.14/TableRow?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import TextField from 'https://esm.sh/@mui/material@5.16.14/TextField?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Toolbar from 'https://esm.sh/@mui/material@5.16.14/Toolbar?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import Typography from 'https://esm.sh/@mui/material@5.16.14/Typography?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
+import useMediaQuery from 'https://esm.sh/@mui/material@5.16.14/useMediaQuery?deps=@mui/system@5.16.14,@mui/utils@5.16.14,react@18.3.1,@emotion/react@11.14.0,@emotion/styled@11.14.1&target=es2022'
 
 const html = htm.bind(React.createElement)
 
